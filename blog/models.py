4542fecd939f0988304e85257c652a14366f7c11
aziz <azizbek4595@gmail.com>
2019-04-16 20:52:02 +0300

    Added bootstrap and jobs are showing up

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+# Create a Blog models
+# title
+# pub_date
+# body
+# image
+
+# Add the Blog app to the settings
+
+# Create a migration
+
+# Mirate
+
+# Add to the admin
