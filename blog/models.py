8cbeb756bcaf1f4fe63148ea05b94f689e5a1a85
aziz <azizbek4595@gmail.com>
2019-04-18 14:58:49 +0300

    Blog functionality and polish

--- blog/models.py
+++ blog/models.py
@@ -1,15 +1,16 @@
 from django.db import models
 
-# Create a Blog models
-# title
-# pub_date
-# body
-# image
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
 
-# Add the Blog app to the settings
+    def __str__(self):
+        return self.title
 
-# Create a migration
+    def summary(self):
+        return self.body[:100]
 
-# Mirate
-
-# Add to the admin
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
